58d2cdf92877195ca3448a4e5bbb535ee925d17f
Ayush <viridescentyt@gmail.com>
2025-05-25 22:50:22 +0530

    Add login screen (demo)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,100 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Button,
+  ActivityIndicator,
+} from "react-native";
+
+import { FirebaseError } from "firebase/app";
+import auth from "@react-native-firebase/auth";
 
 export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      await auth().createUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      const err = e as FirebaseError;
+      alert("Registration failed: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signIn = async () => {
+    setLoading(true);
+
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (e) {
+      const err = e as FirebaseError;
+      alert("Login failed: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <Button title="Login" onPress={signIn} color="green" />
+        )}
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <Button title="Signup" onPress={signUp} color="blue" />
+        )}
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  button: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+  },
+});
